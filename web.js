50809d94005de8963db819e02297644e60228bbd
Jonathan Shih <contact@jonathan-shih.com>
2013-07-12 05:59:32 +0000

    test web.js

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer("Test", "utf-8");
+
+var fs = require('fs');
+//read to buffer
+var data = fs.readFileSync('./index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString("utf-8", 0, 12));
 });
 
 var port = process.env.PORT || 5000;
