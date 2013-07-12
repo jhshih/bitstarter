3eeb272b270fe48cddd8e8dcc277fd9ea91f5766
Jonathan Shih <contact@jonathan-shih.com>
2013-07-12 06:02:56 +0000

    removed char limit in buffer

--- web.js
+++ web.js
@@ -1,15 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer("Test", "utf-8");
-
-var fs = require('fs');
 //read to buffer
 var data = fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
-  response.send(data.toString("utf-8", 0, 12));
+  response.send(data.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
